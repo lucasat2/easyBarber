088f7ddb0d4678e214804a49f17a4de1e41fa35d
Lucas <lucaspaivacontato@gmail.com>
2025-03-20 10:22:49 -0300

    feat: add SQL script for users table creation, data insertion and reset database

---
+++ src/db/insertData.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name, surname, company_name, email, password) 
+VALUES 
+('João', 'Silva', 'Barbearia Elite', 'joao.silva@email.com', 'Test123@'),
+('Maria', 'Santos', 'Barbearia Luxo', 'maria.santos@email.com', 'Test123&'),
+('Carlos', 'Oliveira', 'Barber Style', 'carlos.oliveira@email.com', 'Test123*');
