a59beb1280b93309dce7917c9d35188231ee66dc
Bruno <bruno.lorenzi.catunda@gmail.com>
2025-03-22 19:01:39 -0300

    remove: app.get all

--- src/app.js
+++ src/app.js
@@ -14,10 +14,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", routes);
 
-app.get("*", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "public", "index.html"));
-});
-
 app.listen(port, () => {
   console.log(`Server running on "http://${hostname}:${port}"`);
 });
