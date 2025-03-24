00efa945f5cb1e1478c1f4f93dd32f515cd65de5
Lucas <lucaspaivacontato@gmail.com>
2025-03-23 22:14:14 -0300

    refactor: db index config file

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,12 @@
 const { Pool } = require("pg");
-require("dotenv").config();
+const config = require("../config");
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  user: config.DB_USER,
+  host: config.DB_HOST,
+  database: config.DB_NAME,
+  password: config.DB_PASSWORD,
+  port: config.DB_PORT,
 });
 
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+module.exports = pool;
