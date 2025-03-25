6834ee3b29ce1f81f6eb683723b5dd3dd0859179
santosfabin <fsfabianosantos03@gmail.com>
2025-03-24 21:18:16 -0300

    feat: get all Appointments by employee in db

--- src/services/appointmentsServices.js
+++ src/services/appointmentsServices.js
@@ -1,31 +1,44 @@
 const appointmentsRepository = require("../repository/appointmentsRepository.js");
 
-const createAppointment = async (
-  employeeId,
-  serviceId,
-  date,
-  clientId,
-  startTime,
-  observation
-) => {
-  try {
-    const result = await appointmentsRepository.insertNewAppointment(
-      employeeId,
-      serviceId,
-      date,
-      clientId,
-      startTime,
-      observation
-    );
+const listAllAppointmentsByEmployee = async employeeId => {
+	try {
+		const result = await appointmentsRepository.getAllAppointmentsByEmployee(
+			employeeId
+		);
 
-    if (result) {
-      result;
-    }
-  } catch (error) {
-    throw error;
-  }
+		return result;
+	} catch (e) {
+		throw e;
+	}
+};
+
+const createAppointment = async (
+	employeeId,
+	serviceId,
+	date,
+	clientId,
+	startTime,
+	observation
+) => {
+	try {
+		const result = await appointmentsRepository.insertNewAppointment(
+			employeeId,
+			serviceId,
+			date,
+			clientId,
+			startTime,
+			observation
+		);
+
+		if (result) {
+			result;
+		}
+	} catch (error) {
+		throw error;
+	}
 };
 
 module.exports = {
-  createAppointment,
+	createAppointment,
+	listAllAppointmentsByEmployee
 };
