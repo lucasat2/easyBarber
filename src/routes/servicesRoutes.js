4b110bacc1c5115f2d8c4f124634c761999aab49
Lucas <lucaspaivacontato@gmail.com>
2025-03-28 00:52:54 -0300

    feat: add middlewares to services routes

--- src/routes/servicesRoutes.js
+++ src/routes/servicesRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const servicesController = require("../controller/servicesController");
+const { verifyActiveSession } = require("../middleware");
 
-router.post("/", servicesController.createService);
-router.get("/", servicesController.listAllCompanyServices);
-router.put("/", servicesController.updateService);
-router.delete("/", servicesController.deleteService);
+router.post("/", verifyActiveSession, servicesController.createService);
+router.get("/", verifyActiveSession, servicesController.listAllCompanyServices);
+router.put("/", verifyActiveSession, servicesController.updateService);
+router.delete("/", verifyActiveSession, servicesController.deleteService);
 
 module.exports = router;
