7e3132fe85cb1c0729421675f897a34c99605c0b
Gabriell Queiroz <gabriellqs10@gmail.com>
2025-04-03 16:24:27 -0300

    chore: reorganize folder structure and SQL scripts

--- src/db/insertData.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (name, surname, company_name, email, password) 
-VALUES 
-('João', 'Silva', 'Barbearia Elite', 'joao.silva@email.com', 'Test123@'),
-('Maria', 'Santos', 'Barbearia Luxo', 'maria.santos@email.com', 'Test123&'),
-('Carlos', 'Oliveira', 'Barber Style', 'carlos.oliveira@email.com', 'Test123*');
