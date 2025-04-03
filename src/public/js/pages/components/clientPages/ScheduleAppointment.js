90e5f6a0b835f1bbec1172289a32e28fa3b7faa9
santosfabin <fsfabianosantos03@gmail.com>
2025-04-02 23:23:23 -0300

    feat: fineshed route for the customer to schedule an order

--- src/public/js/pages/components/clientPages/ScheduleAppointment.js
+++ src/public/js/pages/components/clientPages/ScheduleAppointment.js
@@ -1,10 +1,12 @@
 import Header from "./Header.js";
 import ServicesPage from "./ServicesPage.js";
+import ConfirmScheduling from "./ConfirmScheduling.js";
 
-function navigateTo(pageFunction) {
+function navigateTo(pageFunction, obj) {
 	const root = document.getElementById("root");
 	root.innerHTML = "";
-	root.appendChild(pageFunction());
+	root.appendChild(Header(objCompany.companyName));
+	root.appendChild(pageFunction(obj));
 }
 
 function fetchStaffServices(idCompany, idService) {
@@ -54,6 +56,7 @@ function fetchCompanyServices(idCompany) {
 			throw error; // Lança o erro para que o chamador possa tratá-lo
 		});
 }
+let objCompany = null;
 
 export default function ScheduleAppointment() {
 	const root = document.getElementById("root");
@@ -87,6 +90,8 @@ export default function ScheduleAppointment() {
 				// Se a empresa for encontrada, atualiza o título
 				const companyName = data.result;
 				const header = Header(companyName);
+				objCompany = {idCompany, companyName};
+
 				container.appendChild(header);
 				buildContent(container); // Chama a função que monta o conteúdo da página
 			} else {
@@ -102,6 +107,15 @@ export default function ScheduleAppointment() {
 		const idService = urlParams.get("idService");
 
 		const content = document.createElement("div");
+		let serviceNames = "";
+		let time = "";
+		let cost = "";
+
+		let lastIdStaff = null;
+		let dateDaySelected = null;
+		let hourSelected = null;
+		let objStaff = null;
+		let objService = null;
 
 		fetchStaffServices(idCompany, idService)
 			.then(staffs => {
@@ -135,20 +149,24 @@ export default function ScheduleAppointment() {
 					"../../../../../assets/externalSchedulingPage/serviceImage.jpeg";
 				imageService.style.width = "100%";
 
+				const containerContent = document.createElement("div");
+
 				const contentServiceInformation = document.createElement("div");
 				contentServiceInformation.innerHTML = fetchCompanyServices(
 					idCompany
 				).then(services => {
-					let serviceNames = "";
-					let time = "";
-					let cost = "";
-
 					services.forEach(service => {
 						if (service.id == idService) {
 							serviceNames = service.name;
 							time = service.average_duration;
 							cost = service.price;
 						}
+						objService = {
+							id: service.id,
+							name: service.name,
+							time: service.average_duration,
+							cost: service.price
+						};
 					});
 
 					contentServiceInformation.innerHTML = `
@@ -199,6 +217,27 @@ export default function ScheduleAppointment() {
 				button.style.borderRadius = "8px";
 				button.style.width = "fit-content";
 
+				button.addEventListener("click", () => {
+					if (
+						lastIdStaff == null ||
+						dateDaySelected == null ||
+						hourSelected == null
+					) {
+						document.getElementById("errorMessage").innerHTML =
+							"Preencha todos os campos";
+						return;
+					}
+					document.getElementById("errorMessage").innerHTML = "";
+					const scheduler = {
+						objStaff,
+						dateDaySelected,
+						hourSelected,
+						objService,
+						objCompany
+					};
+					navigateTo(ConfirmScheduling, scheduler);
+				});
+
 				button.addEventListener("mouseover", () => {
 					button.style.background = "#ffd700";
 				});
@@ -206,6 +245,8 @@ export default function ScheduleAppointment() {
 				button.addEventListener("mouseout", () => {
 					button.style.background = "#DEE33E";
 				});
+				containerContent.style.display = "flex";
+				containerContent.style.flexDirection = "column";
 
 				const selectStaff = document.createElement("div");
 				selectStaff.style.display = "flex";
@@ -224,7 +265,7 @@ export default function ScheduleAppointment() {
 						confirm.style.width = "100%";
 						confirm.style.border = "none";
 						confirm.style.minHeight = "auto";
-						selectStaff.style.width = "100%";
+						containerContent.style.width = "100%";
 					} else {
 						button.style.position = "relative";
 						button.style.bottom = "auto";
@@ -233,7 +274,7 @@ export default function ScheduleAppointment() {
 						confirm.style.width = "270px";
 						confirm.style.borderRight = "1px solid black";
 						confirm.style.minHeight = "500px";
-						selectStaff.style.width = "calc(100% - 300px)";
+						containerContent.style.width = "calc(100% - 300px)";
 					}
 				}
 
@@ -244,11 +285,19 @@ export default function ScheduleAppointment() {
 				handleScreenChange(mediaQuery); // Aplica a configuração inicial
 
 				confirm.appendChild(contentService);
+
+				const errorMessage = document.createElement("div");
+				errorMessage.id = "errorMessage";
+
 				confirm.appendChild(button);
+				confirm.appendChild(errorMessage);
 				div.appendChild(confirm);
 
+				const hoursToWork = document.createElement("div");
+				hoursToWork.id = "hoursToWork";
 				staffs.forEach(staff => {
 					const div = document.createElement("div");
+					div.id = staff.id;
 					div.style.width = "200px";
 					div.style.height = "150px";
 					div.style.background = "#D9D9D9";
@@ -263,11 +312,10 @@ export default function ScheduleAppointment() {
 					div.style.fontWeight = "700";
 
 					const img = document.createElement("img");
-					img.src =
-						"../../../../../assets/externalSchedulingPage/person.png";
+					img.src = "../../../../../assets/externalSchedulingPage/person.png";
 					img.style.height = "70%";
 					img.style.width = "70%";
-          img.style.objectFit = "cover"
+					img.style.objectFit = "cover";
 
 					const paragraph = document.createElement("p");
 					paragraph.textContent = `${staff.name} ${staff.surname}`;
@@ -275,9 +323,227 @@ export default function ScheduleAppointment() {
 					div.appendChild(img);
 					div.appendChild(paragraph);
 
+					// Evento para selecionar apenas um staff por vez
+					div.addEventListener("click", async () => {
+						const calendar = document.getElementById("calendar-input");
+						const idStaff = staff.id;
+						objStaff = {id: staff.id, name: `${staff.name} ${staff.surname}`};
+						lastIdStaff = idStaff;
+						const dateDay = calendar.value;
+
+						try {
+							const response = await fetch(
+								"http://localhost:3000/api/customer/company/services/staff/schedule",
+								{
+									method: "POST",
+									headers: {
+										"Content-Type": "application/json"
+									},
+									body: JSON.stringify({idStaff: lastIdStaff, date: dateDay})
+								}
+							);
+
+							hourSelected = null;
+							dateDaySelected = dateDay;
+
+							if (!response.ok) {
+								hoursToWork.innerHTML = "Funcionário não trabalha esse dia";
+							}
+
+							hoursToWork.innerHTML = "";
+							const data = await response.json();
+
+							const availableTimes = data.getSchedules.availableTimes.flatMap(
+								({start, end}) => {
+									const slots = [];
+									let startTime = new Date(`2000-01-01T${start}`);
+									const endTime = new Date(`2000-01-01T${end}`);
+
+									while (
+										startTime.getTime() + time * 60000 <=
+										endTime.getTime()
+									) {
+										const nextTime = new Date(
+											startTime.getTime() + time * 60000
+										);
+										slots.push(
+											`${startTime.toTimeString().slice(0, 5)} - ${nextTime
+												.toTimeString()
+												.slice(0, 5)}`
+										);
+										startTime = nextTime;
+									}
+
+									return slots;
+								}
+							);
+
+							availableTimes.forEach(timeSlot => {
+								const listItem = document.createElement("li");
+								listItem.textContent = timeSlot;
+								listItem.style.listStyle = "none";
+								listItem.style.background = "#D9D9D9";
+								listItem.style.width = "8rem";
+								listItem.style.height = "3rem";
+								listItem.style.display = "flex";
+								listItem.style.justifyContent = "center";
+								listItem.style.alignItems = "center";
+								listItem.style.borderRadius = "0.5rem";
+								listItem.style.cursor = "pointer";
+
+								// Evento de clique para mudar a cor do item selecionado
+								listItem.addEventListener("click", () => {
+									// Resetar todos os itens para cinza
+									document.querySelectorAll("#hoursToWork li").forEach(li => {
+										li.style.background = "#D9D9D9";
+										hourSelected = listItem.textContent.split(" - ")[0];
+									});
+
+									// Definir o item clicado como amarelo
+									listItem.style.background = "#DEE33E";
+								});
+
+								hoursToWork.appendChild(listItem);
+								hoursToWork.style.display = "flex";
+								hoursToWork.style.gap = "1rem";
+								hoursToWork.style.flexWrap = "wrap";
+							});
+						} catch (error) {
+							const hoursToWork = document.getElementById("hoursToWork");
+							hoursToWork.innerHTML = "Funcionário não trabalha esse dia";
+
+							hourSelected = null;
+							dateDaySelected = null;
+							console.error("Erro no fetch:", error);
+						}
+
+						// Remove a seleção de todos os outros funcionários
+						document.querySelectorAll(".selected-staff").forEach(el => {
+							el.classList.remove("selected-staff");
+							el.style.background = "#D9D9D9"; // Voltar para a cor padrão
+						});
+
+						// Adiciona a seleção no clicado
+						div.classList.add("selected-staff");
+						div.style.background = "#DEE33E"; // Muda a cor do selecionado
+					});
+
 					selectStaff.appendChild(div);
+					containerContent.appendChild(selectStaff);
 				});
-				div.appendChild(selectStaff);
+
+				const dateInput = document.createElement("input");
+				dateInput.type = "date";
+				dateInput.id = "calendar-input";
+
+				const now = new Date();
+				const year = now.getFullYear();
+				const month = String(now.getMonth() + 1).padStart(2, "0"); // Mês começa do 0, então +1
+				const day = String(now.getDate()).padStart(2, "0");
+
+				const today = `${year}-${month}-${day}`;
+				dateInput.min = today;
+				dateInput.value = today;
+				dateInput.style.width = "fit-content";
+
+				dateInput.addEventListener("change", async () => {
+					const dataDay = dateInput.value;
+
+					if (!lastIdStaff) {
+						const hoursToWork = document.getElementById("hoursToWork");
+						hoursToWork.innerHTML = "Selecione um funcionário";
+						return;
+					}
+
+					try {
+						const response = await fetch(
+							"http://localhost:3000/api/customer/company/services/staff/schedule",
+							{
+								method: "POST",
+								headers: {
+									"Content-Type": "application/json"
+								},
+								body: JSON.stringify({idStaff: lastIdStaff, date: dataDay})
+							}
+						);
+
+						hourSelected = null;
+						dateDaySelected = dataDay;
+
+						if (!response.ok) {
+							throw new Error("Erro ao enviar dados");
+						}
+
+						hoursToWork.innerHTML = "";
+						const data = await response.json();
+
+						const availableTimes = data.getSchedules.availableTimes.flatMap(
+							({start, end}) => {
+								const slots = [];
+								let startTime = new Date(`2000-01-01T${start}`);
+								const endTime = new Date(`2000-01-01T${end}`);
+
+								while (
+									startTime.getTime() + time * 60000 <=
+									endTime.getTime()
+								) {
+									const nextTime = new Date(startTime.getTime() + time * 60000);
+									slots.push(
+										`${startTime.toTimeString().slice(0, 5)} - ${nextTime
+											.toTimeString()
+											.slice(0, 5)}`
+									);
+									startTime = nextTime;
+								}
+
+								return slots;
+							}
+						);
+
+						availableTimes.forEach(timeSlot => {
+							const listItem = document.createElement("li");
+							listItem.textContent = timeSlot;
+							listItem.style.listStyle = "none";
+							listItem.style.background = "#D9D9D9";
+							listItem.style.width = "8rem";
+							listItem.style.height = "3rem";
+							listItem.style.display = "flex";
+							listItem.style.justifyContent = "center";
+							listItem.style.alignItems = "center";
+							listItem.style.borderRadius = "0.5rem";
+							listItem.style.cursor = "pointer";
+
+							// Evento de clique para mudar a cor do item selecionado
+							listItem.addEventListener("click", () => {
+								// Resetar todos os itens para cinza
+								document.querySelectorAll("#hoursToWork li").forEach(li => {
+									li.style.background = "#D9D9D9";
+									hourSelected = listItem.textContent.split(" - ")[0];
+								});
+
+								// Definir o item clicado como amarelo
+								listItem.style.background = "#DEE33E";
+							});
+
+							hoursToWork.appendChild(listItem);
+							hoursToWork.style.display = "flex";
+							hoursToWork.style.gap = "1rem";
+							hoursToWork.style.flexWrap = "wrap";
+						});
+					} catch (error) {
+						const hoursToWork = document.getElementById("hoursToWork");
+						hoursToWork.innerHTML = "Funcionário não trabalha esse dia";
+
+						hourSelected = null;
+						dateDaySelected = null;
+						console.error("Erro no fetch:", error);
+					}
+				});
+
+				containerContent.appendChild(dateInput);
+				containerContent.appendChild(hoursToWork);
+
+				div.appendChild(containerContent);
 
 				content.appendChild(div);
 			})
