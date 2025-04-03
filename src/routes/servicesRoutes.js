a47072957b1b8e4636c3d7e51019b59342852ef8
Gabriell Queiroz <gabriellqs10@gmail.com>
2025-04-03 10:40:42 -0300

    feat: add middleware for active session verification

--- src/routes/servicesRoutes.js
+++ src/routes/servicesRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const servicesController = require("../controller/servicesController");
+const { verifyActiveSession } = require("../middleware");
 
-router.post("/", servicesController.createService);
-router.get("/", servicesController.listAllCompanyServices);
-router.get("/:id", servicesController.getServiceData);
-router.put("/", servicesController.updateService);
-router.delete("/", servicesController.deleteService);
+router.post("/", verifyActiveSession, servicesController.createService);
+router.get("/", verifyActiveSession, servicesController.listAllCompanyServices);
+router.get("/:id", verifyActiveSession, servicesController.getServiceData);
+router.put("/", verifyActiveSession, servicesController.updateService);
+router.delete("/", verifyActiveSession, servicesController.deleteService);
 
 module.exports = router;
