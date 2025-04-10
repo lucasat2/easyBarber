5ddc2303d338c4dcc28db3650d136199316339ef
Gabriell Queiroz <gabriellqs10@gmail.com>
2025-04-10 12:40:08 -0300

    feat: improve service form UX by adding field labels

--- src/public/js/pages/components/serviceComponent/ServiceModal.js
+++ src/public/js/pages/components/serviceComponent/ServiceModal.js
@@ -30,8 +30,14 @@ function ServiceModal(isEdit, serviceData) {
   modalBody.classList.add("serviceModalBody");
   modalContent.appendChild(modalBody);
 
+  const nameInputLabel = document.createElement("label");
+  nameInputLabel.setAttribute("for", "serviceName");
+  nameInputLabel.innerText = "Nome:";
+  modalBody.appendChild(nameInputLabel);
+
   const nameInput = document.createElement("input");
   nameInput.placeholder = "Nome";
+  nameInput.id = "serviceName";
   nameInput.classList.add("serviceNameModalInput");
   modalBody.appendChild(nameInput);
 
@@ -39,18 +45,38 @@ function ServiceModal(isEdit, serviceData) {
   priceAndDurationFields.classList.add("serviceModalPriceAndDurationFields");
   modalBody.appendChild(priceAndDurationFields);
 
+  const priceInputLabel = document.createElement("label");
+  priceInputLabel.setAttribute("for", "servicePrice");
+  priceInputLabel.innerText = "Preço:";
+  priceInputLabel.classList.add("serviceInputLabel");
+  priceAndDurationFields.appendChild(priceInputLabel);
+
   const priceInput = document.createElement("input");
   priceInput.placeholder = "Preço";
+  priceInput.id = "servicePrice";
   priceInput.classList.add("servicePriceAndDurationModalInputs");
-  priceAndDurationFields.appendChild(priceInput);
+  priceInputLabel.appendChild(priceInput);
+
+  const durationInputLabel = document.createElement("label");
+  durationInputLabel.setAttribute("for", "serviceDuration");
+  durationInputLabel.innerText = "Duração (minutos):";
+  durationInputLabel.classList.add("serviceInputLabel");
+  priceAndDurationFields.appendChild(durationInputLabel);
 
   const durationInput = document.createElement("input");
   durationInput.placeholder = "Duração (minutos)";
+  durationInput.id = "serviceDuration";
   durationInput.classList.add("servicePriceAndDurationModalInputs");
-  priceAndDurationFields.appendChild(durationInput);
+  durationInputLabel.appendChild(durationInput);
+
+  const descriptionInputLabel = document.createElement("label");
+  descriptionInputLabel.setAttribute("for", "serviceDescription");
+  descriptionInputLabel.innerText = "Descrição:";
+  modalBody.appendChild(descriptionInputLabel);
 
   const descriptionInput = document.createElement("textarea");
   descriptionInput.placeholder = "Descrição";
+  descriptionInput.id = "serviceDescription";
   descriptionInput.classList.add("serviceModalTextArea");
   modalBody.appendChild(descriptionInput);
 
@@ -64,88 +90,59 @@ function ServiceModal(isEdit, serviceData) {
 
   if (isEdit) {
     nameInput.value = serviceData.name;
-    priceInput.value = serviceData.price;
+    priceInput.value = serviceData.price.replace(".", ",");
     durationInput.value = serviceData.average_duration;
     descriptionInput.value = serviceData.description;
   }
 
   function saveService(isEditable) {
+    const name = nameInput.value.trim();
+    const price = priceInput.value.trim().replace(",", ".");
+    const duration = durationInput.value.trim();
+    const description = descriptionInput.value.trim();
+
+    const method = isEditable ? "PUT" : "POST";
+    const body = {
+      name,
+      description,
+      price,
+      averageDuration: duration,
+    };
+
     if (isEditable) {
-      fetch("/api/services", {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          serviceID: serviceData.id,
-          name: nameInput.value,
-          description: descriptionInput.value,
-          price: priceInput.value,
-          averageDuration: durationInput.value,
-        }),
-      })
-        .then((response) => {
-          if (!response.ok) {
-            return response.json().then((errorData) => {
-              throw new Error(errorData.error || "Falha Desconhecida");
-            });
-          }
-
-          return response.json();
-        })
-        .then((data) => {
-          containerModal.remove();
-
-          const serviceDashboard = ServiceDashboard();
-
-          const main = document.getElementById("main");
-          main.innerHTML = "";
-          main.style.padding = "30px";
-          main.appendChild(serviceDashboard);
-
-          MessageNotification(data.message, " #28a745");
-        })
-        .catch((error) => {
-          MessageNotification(error.message, "#ff6347");
-        });
-    } else {
-      fetch("/api/services", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name: nameInput.value,
-          description: descriptionInput.value,
-          price: priceInput.value,
-          averageDuration: durationInput.value,
-        }),
-      })
-        .then((response) => {
-          if (!response.ok) {
-            return response.json().then((errorData) => {
-              throw new Error(errorData.error || "Falha Desconhecida");
-            });
-          }
-
-          return response.json();
-        })
-        .then((data) => {
-          containerModal.remove();
-
-          const serviceDashboard = ServiceDashboard();
-
-          const main = document.getElementById("main");
-          main.innerHTML = "";
-          main.style.padding = "30px";
-          main.appendChild(serviceDashboard);
-
-          MessageNotification(data.message, " #28a745");
-        })
-        .catch((error) => {
-          MessageNotification(error.message, "#ff6347");
-        });
+      body.serviceID = serviceData.id;
     }
+
+    fetch("/api/services", {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          return response.json().then((errorData) => {
+            throw new Error(errorData.error || "Falha Desconhecida");
+          });
+        }
+
+        return response.json();
+      })
+      .then((data) => {
+        containerModal.remove();
+
+        const serviceDashboard = ServiceDashboard();
+        const main = document.getElementById("main");
+        main.innerHTML = "";
+        main.style.padding = "30px";
+        main.appendChild(serviceDashboard);
+
+        MessageNotification(data.message, "#28a745");
+      })
+      .catch((error) => {
+        MessageNotification(error.message, "#ff6347");
+      });
   }
 
   document.body.appendChild(containerModal);
